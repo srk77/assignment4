334c66a52c3399ca384f3032c9c6ff0d75473a98
Shweta R Kumthekar <srk77@afsconnect1.njit.edu>
2017-10-02 16:32:23 -0400

    string compare

--- index.php
+++ index.php
@@ -5,6 +5,14 @@ echo "The value of \$date: ".$date."<br>";
 $date1 = str_replace("-","/",$date);
 echo('The value of \$date1: '.$date1.'<br>');
 
+if(strcmp($date1,$tar) >0 ){
+echo "the future<br> ";
+} else if(strcmp($date1,$tar)==0){
+echo "oops<br>";
+} else {
+echo "the past<br>";
+}
+
 $tar = "2017/05/24";
 echo "The value of \$tar: ".$tar."<br>";
 
